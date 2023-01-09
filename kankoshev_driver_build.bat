4199dbb2f17239d55600a85b5983545f3e1b5231
Autoplay1999 <autoplay1999@hotmail.com>
2023-01-09 17:37:20 +0700

    + build command

---
+++ kankoshev_driver_build.bat
@@ -0,0 +1,7 @@
+@echo off
+call "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
+MSBuild face_injector_v2.sln /t:kankoshev_driver:Clean;kankoshev_driver:Rebuild /p:Configuration=Release;Platform=x64
+MSBuild face_injector_v2.sln /t:kankoshev_driver:Clean;kankoshev_driver:Rebuild /p:Configuration=Debug;Platform=x64
+MSBuild face_injector_v2.sln /t:kankoshev_driver:Clean;kankoshev_driver:Rebuild /p:Configuration=Release;Platform=x86
+MSBuild face_injector_v2.sln /t:kankoshev_driver:Clean;kankoshev_driver:Rebuild /p:Configuration=Debug;Platform=x86
+rd .vs /S /Q
